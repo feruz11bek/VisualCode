8bea6550637fd078500ecf9d60a15dee101d8b53
feruz11bek <92453808+feruz11bek@users.noreply.github.com>
2022-04-14 08:59:22 +0500

    (m xn) matritsaning eng kichik elementi turgan  satrni olib tashlab chiqarish
    Signed-off-by: feruz11bek <92453808+feruz11bek@users.noreply.github.com>

---
+++ java38.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class java38 {
+    public static void main(String [] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int m,n,k,h=0;
+        System.out.print("m=");
+        m=in.nextInt();
+        System.out.print("n=");
+        n=in.nextInt();
+        int a [][] = new int [m][n];
+        System.out.println("a[][]=");
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            a[i][j]=in.nextInt();
+        }
+        System.out.println();
+        k=a[0][0];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(k>a[i][j])
+                {
+                    k=a[i][j];h=i;
+                }
+            }
+        }
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+           {
+               if(h==i) continue;
+               System.out.print(a[i][j]+ " ");
+           }
+           System.out.println();
+        }
+    }
+}
